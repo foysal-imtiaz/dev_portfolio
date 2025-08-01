f12b6d3c82f61874607dc0ae5aa65c70536dc9bc
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-08-01 13:18:25 +0600

    added motion to intro section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { motion } from "framer-motion";
 
+// NAVBAR LINKS
 const navLinks = [
   { name: "Projects", path: "/projects" },
   { name: "Posts", path: "/posts" },
@@ -9,6 +10,14 @@ const navLinks = [
   { name: "Contact", path: "/contact" },
 ];
 
+// MOTION Animation Values
+const transition = { duration: 0.7, ease: [0.25, 0.1, 0.25, 1] };
+const variants = {
+  hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
+  visible: { filter: "blur(0)", transform: "translateY(0)", opacity: 1 },
+};
+
+// MAIN Component
 const Navbar = () => {
   const [hovered, setHovered] = useState(null);
   const [menuOpen, setMenuOpen] = useState(false);
@@ -17,6 +26,7 @@ const Navbar = () => {
     setMenuOpen((prev) => !prev);
   };
 
+  // Function to Handle Scroll
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
@@ -31,20 +41,26 @@ const Navbar = () => {
     <div
       className={`sticky top-0 px-6 pb-4 pt-3 text-xl w-full md:w-[94%] mx-auto border-b-[1px] border-gray-200 md:border-b-0 md:border-r-[1px] md:border-l-[1px] md:border-opacity-70 md:border-gray-200 transition-all duration-300 z-50 ${
         scrolled
-          ? "bg-white/80 backdrop-blur-sm shadow-sm px-[8px] pb-1 pt-1 md:w-[82%] mx-auto top-3 rounded-full"
+          ? "bg-[#ffffff] md:bg-white/80 md:backdrop-blur-sm md:shadow-sm md:px-[5px] md:pb-[3px] md:pt-[4px] md:w-[78%] mx-auto md:top-3 md:rounded-full"
           : "bg-[#ffffff] px-6 pb-4 pt-3 w-full md:w-[94%] mx-auto"
       }`}
     >
-      <div className="flex justify-between items-center">
-        <div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        transition={{ staggerChildren: 0.04 }}
+        className="flex justify-between items-center"
+      >
+        <motion.div transition={transition} variants={variants}>
           <Link to="/">
             <img
               src="./src/assets/image2color.jpg"
               alt="profile picture"
-              className="h-10 w-10 rounded-full"
+              className={`h-10 w-10 rounded-full
+                ${scrolled ? "md:h-8 md:w-8" : "h-10 w-10 rounded-full"}`}
             />
           </Link>
-        </div>
+        </motion.div>
         {/* DESKTOP VIEW */}
         <div className="hidden md:flex  justify-center items-center text-sm text-neutral-800">
           {navLinks.map((link, index) => (
@@ -61,7 +77,13 @@ const Navbar = () => {
                   className="h-full w-full absolute inset-0 rounded-md bg-neutral-100"
                 />
               )}
-              <span className="relative z-10">{link.name}</span>
+              <motion.span
+                transition={transition}
+                variants={variants}
+                className="relative z-10"
+              >
+                {link.name}
+              </motion.span>
             </Link>
           ))}
         </div>
@@ -89,7 +111,7 @@ const Navbar = () => {
             ))}
           </div>
         )}
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,58 @@
 import { motion } from "framer-motion";
+
+const transition = { duration: 0.8, ease: [0.25, 0.1, 0.25, 1] };
+const variants = {
+  hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
+  visible: { filter: "blur(0)", transform: "translateY(0)", opacity: 1 },
+};
+
 const HomePage = () => {
   return (
     <div>
       {/* INTRO SECTION */}
-      <div className="">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        transition={{ staggerChildren: 0.04 }}
+        className=""
+      >
         <div className="md:flex md:justify-start items-center gap-4">
-          <span className="md:hidden text-neutral-700 border-[1px] border-gray-300 opacity-70 py-1 px-2 shadow-sm rounded-md text-sm">
+          <motion.span
+            transition={transition}
+            variants={variants}
+            className="md:hidden text-neutral-700 border-[1px] border-gray-300 opacity-70 py-[2px] px-2 shadow-sm rounded-md text-sm"
+          >
             Full Stack Dev
-          </span>
-          <h1 className="mt-1 text-shadow-sm text-4xl flex font-extrabold text-neutral-700">
+          </motion.span>
+          <motion.h1
+            transition={transition}
+            variants={variants}
+            className="mt-1 text-shadow-sm text-4xl flex font-extrabold text-neutral-700"
+          >
             Foysal Imtiaz
-          </h1>
-          <span className="hidden text-neutral-700 md:flex md:border-[1px] md:h-fit md:border-gray-300 md:opacity-70 md:shadow-sm md:py-1 md:px-2 md:rounded-md md:text-sm">
+          </motion.h1>
+          <motion.span
+            transition={transition}
+            variants={variants}
+            className="hidden text-neutral-700 md:flex md:border-[1px] md:h-fit md:border-gray-300 md:opacity-70 md:shadow-sm md:py-[2px] md:px-2 md:rounded-md md:text-sm"
+          >
             Full Stack Dev
-          </span>
+          </motion.span>
         </div>
-        <p className="text-neutral-500 mt-4 md:w-[65%]">
+        <motion.p
+          transition={transition}
+          variants={variants}
+          className="text-neutral-500 mt-4 md:w-[65%]"
+        >
           Hi, I am a Full Stack developer proficient in MERN stack and NextJS. I
           love building projects, experimenting with new technologies, and
           sharing my work with the community. I'm also exploring SaaS and its
           potential for innovative solutions. I'm open to collaboration and
           always eager to learn something new!
-        </p>
-      </div>
+        </motion.p>
+      </motion.div>
       <span className="my-10 w-full h-[1px] block bg-gray-200"></span>
+
       {/* PROJECT SECTION */}
       <div>
         <h1 className="text-gray-800">Things I have built so far</h1>
