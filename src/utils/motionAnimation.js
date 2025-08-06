1f7e4e3a1cf9f5b3478bcfaa449d13ae10d0f5f6
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-08-06 21:28:14 +0600

    changed blur to scale to fix keyframe error

--- src/utils/motionAnimation.js
+++ src/utils/motionAnimation.js
@@ -1,5 +1,15 @@
+// previous code that cause keyframe error
+
+// export const transition = { duration: 0.5, ease: [0.25, 0.1, 0.25, 1] };
+// export const variants = {
+//   hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
+//   visible: { filter: "blur(0px)", transform: "translateY(0%)", opacity: 1 },
+// };
+
+// new code that works fine
+
 export const transition = { duration: 0.5, ease: [0.25, 0.1, 0.25, 1] };
 export const variants = {
-  hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
-  visible: { filter: "blur(0px)", transform: "translateY(0%)", opacity: 1 },
+  hidden: { scale: 0.95, transform: "translateY(20%)", opacity: 0 },
+  visible: { scale: 1, transform: "translateY(0%)", opacity: 1 },
 };
