1cb91a3b5436fa0c7d3233b0799d5589e2167955
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-08-10 18:40:39 +0600

    path fix 2

--- src/utils/iconLoader.js
+++ src/utils/iconLoader.js
@@ -1,12 +1,33 @@
-const modules = import.meta.glob("../../assets/icons/*.svg", {
-  eager: true,
-  query: "?url",
-  import: "default",
-});
+// const modules = import.meta.glob("/assets/icons/*.svg", {
+//   eager: true,
+//   query: "?url",
+//   import: "default",
+// });
 
+// export const icons = Object.fromEntries(
+//   Object.entries(modules).map(([filePath, url]) => {
+//     const name = filePath.split("/").pop().replace(".svg", "");
+//     return [name, url];
+//   })
+// );
+
+// src/utils/icon.js
+
+// List your icons here without the ".svg" extension
+const iconNames = [
+  "Express",
+  "HTML5",
+  "JavaScript",
+  "MongoDB",
+  "MySQL",
+  "Next.js",
+  "Node.js",
+  "React",
+  "Tailwind CSS",
+  // ...add more icon names as needed
+];
+
+// Create an object mapping { iconName: "/assets/icons/iconName.svg" }
 export const icons = Object.fromEntries(
-  Object.entries(modules).map(([filePath, url]) => {
-    const name = filePath.split("/").pop().replace(".svg", "");
-    return [name, url];
-  })
+  iconNames.map((name) => [name, `/assets/icons/${name}.svg`])
 );
