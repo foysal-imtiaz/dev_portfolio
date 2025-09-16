23bf52e1f98b882ca4857933690a24d65ee4ed12
foysal-imitaz <foysalimtiaz86@gmail.com>
2025-09-16 16:02:19 +0600

    menu close in mobile fix

--- src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children }) => {
-  return (
-    <div className="dark:bg-[#171717] flex-1 border-l-[1px] border-r-[1px] border-gray-200 dark:border-[#222222] border-opacity-70 w-[94%] mx-auto bg-[#ffffff]  pt-4">
-      {children}
-    </div>
-  );
-};
-
-export default Container;
